905a2beed9209b79aa5f230f5da242d9933c9380
Ofentse Moeketsi Mogoatlhe <u14254922@tuks.co.za>
2020-06-19 00:31:57 +0200

    remove duplicate files

--- Traffic Light Website/node_modules/@influxdata/influxdb-client/dist/impl/node/nodeChunkCombiner.d.ts
+++
@@ -1,4 +0,0 @@
-import { ChunkCombiner } from '../../transport';
-declare const nodeChunkCombiner: ChunkCombiner;
-export default nodeChunkCombiner;
-//# sourceMappingURL=nodeChunkCombiner.d.ts.map
--- Traffic Light Website/node_modules/@influxdata/influxdb-client/dist/impl/pureJsChunkCombiner.d.ts
+++
@@ -1,4 +0,0 @@
-import { ChunkCombiner } from '../transport';
-declare const pureJsChunkCombiner: ChunkCombiner;
-export default pureJsChunkCombiner;
-//# sourceMappingURL=pureJsChunkCombiner.d.ts.map
