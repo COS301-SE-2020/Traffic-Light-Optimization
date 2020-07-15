9eef8016687db8e6dd097e8a3893d94c312d2cfc
Richard Sithole <u15360441@tuks.co.za>
2020-07-15 03:14:00 +0200

    Add historical data

--- tl_optimization/main_app/urls.py
+++ tl_optimization/main_app/urls.py
@@ -4,8 +4,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     #path('', views.index, name='index'),
-    path('', views.create, name='index'),
-    path('data', views.upload, name='data'),
+    path('', views.upload, name='index'),
+    # path('data', views.upload, name='data'),
     path('simulation/', views.simulation, name='simulation'),
     path('home', views.home, name='home'),
 ]
