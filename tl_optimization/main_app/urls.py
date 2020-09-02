7eb95a82a800f5994c3418ee26beee508dd9226c
Kgatishi <u15209190@tuks.co.za>
2020-09-02 21:27:34 +0200

    Streaming network visualization and simulation

--- tl_optimization/main_app/urls.py
+++ tl_optimization/main_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     #path('', views.index, name='index'),
@@ -37,3 +39,4 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
